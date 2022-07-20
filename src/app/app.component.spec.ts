b3dcc992e328ca678b93419dda66a1c8f28ed155
manininycolas <manininycolas@gmail.com>
2022-07-20 15:56:09 -0300

    teste-servico-calculadoraservice

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { CalculadoraModule} from './calculadora';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        CalculadoraModule
       ],
       declarations: [
         AppComponent
